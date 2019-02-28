39195597fda5f577b96548a7906cce96f3317621
scott <scott.m.burdick@gmail.com>
2019-02-28 16:07:30 -0500

    Add Rack and App

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,13 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.newres['Content-Type'] = 'text/html'
+  res.write("Hello world!")
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
